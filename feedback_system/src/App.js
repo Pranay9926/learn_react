08acaea9d4589469f40d579deabfa52ffad5dbe1
tarun-hiteshi <tarun.khathuriya@hiteshi.com>
2023-05-24 19:48:09 +0530

    pushed new code

--- feedback_system/src/App.js
+++ feedback_system/src/App.js
@@ -1,14 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import { Login } from './pages/loginpage';
-import { Forms } from './pages/formpage';
-import FeedbackForm from './pages/feedback';
+import logo from "./logo.svg";
+import "./App.css";
+import { Login } from "./pages/loginpage";
+import { Forms } from "./pages/formpage";
+import FeedbackForm from "./pages/feedback";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <Login/> */}
-      {/* <Forms/> */}
-      <FeedbackForm/>
+      <nav class="navbar">
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <div style={{ marginLeft: "700px", marginTop: "20px" }}>
+            <h1
+              style={{
+                textAlign: "center",
+                color: "white",
+                padding: "10px",
+                display: "inline",
+                margin: "auto",
+              }}
+            >
+              Welcome to Student Feedback System
+            </h1>
+          </div>
+          <div>
+            <ul class="nav-list">
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+              <li>
+                <Link to="/Forms">Forms</Link>
+              </li>
+              <li>
+                <Link to="/FeedbackForm">FeedbackForm</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element=<Login /> />
+        <Route path="/Forms" element=<Forms /> />
+        <Route path="/FeedbackForm" element=<FeedbackForm /> />
+      </Routes>
     </div>
   );
 }
--- feedback_system/src/pages/loginpage.js
+++ feedback_system/src/pages/loginpage.js
@@ -1,49 +1,53 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 export const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle login logic here, such as validating the credentials or making an API request
-        axios.post('http://localhost:3001/login',{email,password})
-        .then((res)=>{
-            console.log(res)
-        })
-        // console.log('Email:', email);
-        // console.log('Password:', password);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here, such as validating the credentials or making an API request
+    axios
+      .post("http://localhost:3001/login", { email, password })
+      .then((res) => {
+        console.log(res);
+      });
+    // console.log('Email:', email);
+    // console.log('Password:', password);
+  };
 
-    return (
-        <div>
-            <h1 id="loginh1">Welcome to Student Feedback System</h1>
-            <h2 id="login2">Login</h2>
-            <form id="login" onSubmit={handleSubmit}>
-                <h3>Enter your Email ID</h3>
-                <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
-                <h3>Enter your Password</h3>
+  return (
+    <div>
+      <form id="login" onSubmit={handleSubmit}>
+        <h2 id="loginh2">Login</h2>
+        <h3>Enter your Email ID</h3>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <h3>Enter your Password</h3>
 
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                />
-                <br/>
-                <br/>
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
